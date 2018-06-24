9e6f3ca0af9b684e2f8d9d1977499ac63c6ba7dc
Rion <rionirvin>
2018-06-23 20:34:09 -0500

    saturday

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+// Create Game of Thrones Questions in Array's with the correct answer pulling from the Array
+var gameQuestions = [{
+    question: ""
+
+}]
+
+
+
+
+// Create buttons for Start Game - End Game - and Answer Choices 
+// Show / Hide different pages in the on click functions 
+// Tally Correct and Incorrect answers - but hide until end of game 
+// Create countdown for each question. 
+// create a timer to continue on through the game with out any user input 
+// at the end of the game - start new game - DO NOT REFRESH PAGE 
